7289c210bb72148871f5ae39b664e9caf8eb2856
Alec Johnson <alecjohnson55@gmail.com>
2017-07-09 20:17:05 -0700

    add validate, insert, and get methods

--- dynamic_arrays/alec_johnson_2/lib/static_array.rb
+++ dynamic_arrays/alec_johnson_2/lib/static_array.rb
@@ -2,16 +2,28 @@
 class StaticArray
   def initialize(length)
     @store = Array.new(length)
+    @length = length
   end
 
   # O(1)
   def [](index)
+    validate!(index)
+    @store[index]
   end
 
   # O(1)
   def []=(index, value)
+    validate!(index)
+    @store[index] = value
   end
 
   protected
+
   attr_accessor :store
+
+  private
+
+  def validate!(index)
+    raise "index out of bounds" unless index < @length && index >= 0
+  end
 end
